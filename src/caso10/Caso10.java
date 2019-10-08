ce936fed0ad130ddcbaa0f198c353e0aa063d01f
BrayanMQ <brayanmq11288@gmail.com>
2019-10-08 16:16:18 -0600

    Commit de prueba en dev 2

--- src/caso10/Caso10.java
+++ src/caso10/Caso10.java
@@ -4,7 +4,7 @@ package caso10;
 public class Caso10 {
 
     public static void main(String[] args) {
-        System.out.println("Commit de prueba.");
+        System.out.println("Commit de prueba");
     }
     
 }
