36a016283e8a6e62b33f3e3e67f64a74e0bb601a
RonnyJB <Eg@EgO>
2019-10-09 23:03:56 -0600

    Agregar atributo padre en Nodo.

--- src/model/Nodo.java
+++ src/model/Nodo.java
@@ -7,10 +7,20 @@ public class Nodo {
     
     private Sensor sensor;
     private ArrayList<Nodo> listaHijos;
+    private Nodo padre;
 
-    public Nodo(Sensor pSensor) {
+    public Nodo(Sensor pSensor, Nodo pPadre) {
         this.sensor = pSensor;
         this.listaHijos = new ArrayList<>();
+        this.padre = pPadre;
+    }
+
+    public Nodo getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Nodo pPadre) {
+        this.padre = pPadre;
     }
 
     public Sensor getSensor() {
